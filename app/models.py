b0d6bd437d7466e1b624b642fbaac64a1e8265c5
Mohit <lalpanda6@gmail.com>
2023-02-04 15:58:01 +0530

    mian functionality of connecting the backend and the frontend is done just done for user  as of now

--- app/models.py
+++ app/models.py
@@ -1,4 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class ques(models.Model):
+    text=models.CharField(max_length=200)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    # Views
+    # Upvotes
+    created_at=models.DateTimeField(auto_now_add=True)
+    # tags=
+    # comments=models.IntegerField()
+    # answers
+
+
+class tags(models.Model):
+    text=models.CharField(max_length=100)
+    
+class comments(models.Model):
+    # (text, author name, created_time
+    text=models.CharField(max_length=200)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    created_time=models.DateTimeField(auto_now_add=True)
+    
+class answers(models.Model):
+    text=models.CharField(max_length=200)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    comments=models
 
